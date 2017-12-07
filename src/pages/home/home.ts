b6b797a11b8291678bb6e201bb092ef60f7312f9
Duncan Corbett <duncanrcorbett@gmail.com>
2017-12-07 10:47:20 +0200

    set up assignment

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,7 +6,8 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  tapped = 0;
+  pressed = 0;
   constructor(public navCtrl: NavController) {
 
   }
