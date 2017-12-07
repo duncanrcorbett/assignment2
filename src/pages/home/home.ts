d3d2ba7c21891dd10e167b3418bf9e2238958615
Duncan Corbett <duncanrcorbett@gmail.com>
2017-12-07 11:22:49 +0200

    finished assignment

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -12,4 +12,31 @@ export class HomePage {
 
   }
 
+  onDidReset(resetType: string){
+    switch (resetType) {
+      case 'tap':
+        this.tapped = 0;
+        break;
+      case 'press':
+        this.pressed = 0;
+        break;
+      default:
+        this.tapped = 0;
+        this.pressed = 0;
+    }
+  }
+
+  onTap(){
+    console.log('tapped');
+    this.tapped++;
+  }
+
+  onPress(){
+    console.log('pressed');
+    this.pressed++;
+  }
+
+  didWin(){
+    return this.tapped == 2 && this.pressed == 4;
+  }
 }
